6857fe74730af2dd92993424ab485bd74d8d6fb5
Andrew Hoetker <andrew@hoetker.engineer>
2021-07-29 23:19:27 -0600

    remove cleanings files

--- glortho/api/routes/__init__.py
+++ glortho/api/routes/__init__.py
@@ -1,11 +1,7 @@
 from fastapi import APIRouter
-from glortho.api.routes.cleanings import (
-    router as cleanings_router,
-)
 from glortho.api.routes.users import (
     router as users_router,
 )
 
 router = APIRouter()
-router.include_router(cleanings_router, prefix="/cleanings", tags=["cleanings"])
 router.include_router(users_router, prefix="", tags=["users"])
